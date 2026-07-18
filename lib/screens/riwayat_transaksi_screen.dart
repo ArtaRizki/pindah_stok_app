7b5fd3c142037b828a49abce714a62e4654d3734
ArtaRizki <yuliartarizkinusantoko@gmail.com>
2026-07-18 16:28:20 +0700

    chore: fix linter warning for multiple underscores

--- lib/screens/riwayat_transaksi_screen.dart
+++ lib/screens/riwayat_transaksi_screen.dart
@@ -264,7 +264,7 @@ class _RiwayatTransaksiScreenState extends State<RiwayatTransaksiScreen> {
                         : ListView.separated(
                             padding: const EdgeInsets.all(16),
                             itemCount: _transaksi.length,
-                            separatorBuilder: (_, __) => const SizedBox(height: 12),
+                            separatorBuilder: (context, index) => const SizedBox(height: 12),
                             itemBuilder: (context, index) {
                               final t = _transaksi[index];
                               
